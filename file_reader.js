fd66e5e6557e2fd8f84ff4e1ebf851091c4149da
yuta-hamasaki <yh02052002@gmail.com>
2024-07-23 16:49:23 -0700

    Completed

--- file_reader.js
+++ file_reader.js
@@ -1,5 +1,32 @@
 const fs = require("fs");
 
-fs.readFile("./firstname.txt", "utf-8", (err, data1) => {
-  // your code here
+fs.readFile("./firstname.txt", "utf-8", (err, fName) => {
+  if(err){
+    console.error("error getting firstname", err)
+  }
+  fs.readFile("./lastname.txt", "utf-8", (err, lName)=>{
+    if(err){
+      console.error("error getting lastname", err)
+    }
+    fs.readFile("./age.txt", "utf-8", (err, age)=>{
+      if(err){
+        console.error("error getting age", err)
+      }
+      fs.readFile("./hobbies.txt", "utf-8", (err, hobbies)=>{
+        if(err){
+          console.error("error getting hobbies", err)
+        }
+
+        const hobbiesStr = hobbies.replace(/^\[|\]$/g, '').replace(/"/g, '');
+        const hobbiesArr = hobbiesStr.split(',').map(hobby => hobby.trim());
+        const arr = [...hobbiesArr];
+        const hobby1 = arr[0]; 
+        const hobby2= arr[1];
+
+        console.log(`${fName} ${lName} is ${age} years old and his hobbies are ${hobby1} and ${hobby2}`)
+      })
+    })
+  })
+
+
 })
